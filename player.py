7e3bc4de0837f9de9146879e189d940f098b0cca
Robert Miller <rmiller003@gmail.com>
2022-02-21 14:10:10 -0500

    Stage 3

--- player.py
+++ player.py
@@ -2,11 +2,12 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos):
+    def __init__(self, pos,constraint,speed):
         super().__init__()
         self.image = pygame.image.load('player.png').convert_alpha()
-        self.rect = self.image.get_rect(midbottom=pos)
-        self.speed = 5
+        self.rect = self.image.get_rect(midbottom= pos)
+        self.speed = speed
+        self.max_x_constraint = constraint
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -16,5 +17,12 @@ class Player(pygame.sprite.Sprite):
         elif keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
 
+    def contains(self):
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.right >= self.max_x_constraint:
+            self.rect.right = self.max_x_constraint
+
     def update(self):
         self.get_input()
+        self.contains()
