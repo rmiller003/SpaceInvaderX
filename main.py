90ce32f250860e9345132d33cdc62791dffc413f
rmill <rmiller003@gmail.com>
2022-03-20 17:25:07 -0400

    Stage 6

--- main.py
+++ main.py
@@ -2,16 +2,43 @@
 
 import pygame, sys
 from player import Player
+import obstacle
 
 class Game:
     def __init__(self):
+        # Player setup
         player_sprite = Player((screen_width / 2,screen_height),screen_width,5)
         self.player = pygame.sprite.GroupSingle(player_sprite)
+
+
+        # Obstacle setup
+        self.shape = obstacle.shape
+        self.block_size = 6
+        self.blocks =pygame.sprite.Group()
+        self.obstacle_amount = 4
+        self.obstacle_x_position = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
+        self.create_multiple_obstacles(*self.obstacle_x_position, x_start = 0, y_start= 400)
+
+    def create_obstacles(self, x_start, y_start,offset_x):
+        for row_index, row in enumerate(self.shape):
+            for col_index,col in enumerate(row):
+                if col == 'x':
+                    x = x_start + col_index * self.block_size + offset_x
+                    y = y_start + row_index * self.block_size
+                    block = obstacle.Block(self.block_size,(241,79,80),x,y)
+                    self.blocks.add(block)
+
+    def create_multiple_obstacles(self,*offset,x_start,y_start):
+        for offset_x in offset:
+            self.create_obstacles(x_start,y_start,offset_x)
+
 
     def run(self):
         self.player.sprite.lasers.draw(screen)
         self.player.update()
         self.player.draw(screen)
+
+        self.blocks.draw(screen)
         # update all sprite groups
         # draw all sprite groups
 
